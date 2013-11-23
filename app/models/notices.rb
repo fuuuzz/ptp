69e32ad76a8103d592e914bdfaba3186af116104
PqBenoit <benoitpaquier001@gmail.com>
2013-11-23 16:59:41 +0100

    Deleting useless files

--- app/models/notices.rb
+++
@@ -1,3 +0,0 @@
-class Notices < ActiveRecord::Base
-  attr_accessible :content, :id_bar, :id_user, :rating
-end
--- test/unit/notices_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class NoticesTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
