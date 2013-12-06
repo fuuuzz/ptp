c61bbd6ac0cad53b23aa22e8cdcfaeabd0ace413
alexben <alexiben7@gmail.com>
2013-12-06 18:23:44 +0100

    Correction classement de bars list

--- app/assets/javascripts/bars.js
+++ app/assets/javascripts/bars.js
@@ -4,3 +4,8 @@ var options = {
 var input = document.getElementById('bar_address');
 autocomplete = new google.maps.places.Autocomplete(input, options);
 
+function checkPosition(){
+    var $inputAddress = $('#bar_address');
+
+
+}
