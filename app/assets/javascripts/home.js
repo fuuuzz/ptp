f59aecc5396fb9f058810ee27d2cff258e9301ed
alexben <alexiben7@gmail.com>
2013-12-06 23:15:11 +0100

    Autocomplete new bar

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -20,6 +20,7 @@ function setupPanelOthers(){
             .done(function( newbar ) {
                 $pannelOthers.append( newbar );
                 $pannelOthers.animate({left: 0}, 300);
+                autocompleteNew();
 
                 $('#close-new').on('click', function(){
                     $pannelOthers.animate({left:'100%'}, 300, function(){
@@ -29,6 +30,24 @@ function setupPanelOthers(){
             });
         })
     }
+}
+
+//autocomplete for new bar form
+function autocompleteNew() {
+    var options = {
+        componentRestrictions: { country: 'fr'}
+    }
+    // Create the search box and link it to the UI element.
+    var input = (document.getElementById('bar_address'));
+
+    var searchBox = new google.maps.places.SearchBox((input,options));
+
+    // Bias the SearchBox results towards places that are within the bounds of the
+    // current map's viewport.
+    google.maps.event.addListener(map, 'bounds_changed', function() {
+        var bounds = map.getBounds();
+        searchBox.setBounds(bounds);
+    });
 }
 
 
--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -282,13 +282,8 @@ function init(){
 
         function searchPlace() {
 
-
             // Create the search box and link it to the UI element.
             var input = (document.getElementById('pac-input'));
-
-//            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-
 
             var searchBox = new google.maps.places.SearchBox((input));
 
