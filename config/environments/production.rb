8843cad7bd2414cbd37c73e297c4e8518cffbc8c
PqBenoit <benoitpaquier001@gmail.com>
2014-01-17 10:39:40 +0100

    Fixing glyphicons issue online

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,6 @@ Payetapinte::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
+  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
   config.action_mailer.default_url_options = { :host => 'payetapinte.heroku.com' }
 end
