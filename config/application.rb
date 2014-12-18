d2c42f7335192c1703152d9ce8139947965848d9
Administrator <admin@local.host>
2014-12-18 10:35:41 +0100

    New York city

--- config/application.rb
+++ config/application.rb
@@ -68,7 +68,7 @@ module Payetapinte
         :port                 => 587,
         :domain               => "payetapinte.fr",
         :user_name            => "bienvenue@payetapinte.fr",
-        :password             => ENV["PASSMAIL"]
+        :password             => "ptp2013mailbar0908",
         :authentication       => :plain,
         :enable_starttls_auto => true
     }
